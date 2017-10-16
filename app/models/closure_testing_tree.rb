95256aaa31eddaef39d08fc3a763334c5df824c8
Erik Hetzner <ehetzner@plos.org>
2017-10-16 10:41:28 -0700

    Fix has_closure_tree_root relation

--- app/models/closure_testing_tree.rb
+++ app/models/closure_testing_tree.rb
@@ -1,5 +1,5 @@
 # cannot call model ClosureTree due to naming collisions with the gem
 class ClosureTestingTree < ActiveRecord::Base
   has_closure_tree
-  has_closure_tree_root :root_node
+  has_closure_tree_root :root_closure_testing_tree
 end
